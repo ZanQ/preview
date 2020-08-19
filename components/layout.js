cca787b9ff1aac77750afb35a1daa860c6a47590
Peter Tang <peter.tang.lai@gmail.com>
2020-08-19 16:43:38 -0600

    First commit

---
+++ components/layout.js
@@ -0,0 +1,34 @@
+import Head from 'next/head'
+import styles from './layout.module.css'
+import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
+
+const name = 'Peter'
+export const siteTitle = 'ZanQ: An Anonymous Personal Stories platform'
+
+export default function Layout({ children, home }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Sometimes the best stories just happen to be real ones."
+        />
+        <meta
+          property="og:image"
+          content="http://zanq.co/images/logo.jpg"
+        />
+        <meta name="og:title" content={siteTitle} />
+      </Head>
+      <main>{children}</main>
+      {!home && (
+        <div className={styles.backToHome}>
+          <Link href="http://dev.zanq.co/Zan/">
+            <a>← Sign Up</a>
+          </Link>
+        </div>
+      )}
+    </div>
+  )
+}
---
+++ url/baseUrl.js
@@ -0,0 +1,5 @@
+//export const URL_BASE = 'http://dev.zanq.co/';
+export const URL_BASE ='http://localhost/ZanQ/';
+
+/* Anonymous User Post Retrieval */
+export const ANON_POST_DETAILS = URL_BASE + "index.php/Api/Post/PostDetailWithIP" /* Retrieve Anonymous Post Details */
